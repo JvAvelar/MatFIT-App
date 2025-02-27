2c797e7553ceb708230f35f533d7ab0617a0b68e
João Vitor <jvitoravelar1200@gmail.com>
2025-02-27 00:28:44 -0300

    implementado o login de usuarios com Firebase na LoginActivity.kt

--- Projeto/app/src/main/java/engsoft/matfit/view/LoginActivity.kt
+++ Projeto/app/src/main/java/engsoft/matfit/view/LoginActivity.kt
@@ -2,7 +2,9 @@ package engsoft.matfit.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import engsoft.matfit.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -10,20 +12,64 @@ class LoginActivity : AppCompatActivity() {
         ActivityLoginBinding.inflate(layoutInflater)
     }
 
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         binding.bntEnter.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            doLogin()
         }
 
         binding.textRegister.setOnClickListener {
             startActivity(Intent(this, CadastroActivity::class.java))
+            finish()
         }
 
         supportActionBar?.hide()
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        verifyUserLogged()
+    }
+
+    // Fazer login com Firebase
+    private fun doLogin() {
+        val email = binding.editEmail.text.toString()
+        val password = binding.editPasswd.text.toString()
+
+        if (email.isEmpty() || password.isEmpty() || password.length < 6) {
+            toast("Email e/ou senha incorretos.")
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener { success ->
+                toast("Seja bem vindo(a) de volta!")
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener { exception -> toast("${exception.message}")
+               // toast("Email e/ou senha incorretos.")
+            }
+    }
+
+    // Verifica se o usuáro está logado após a criação e envia diretamente para a MainActivity se estiver
+    private fun verifyUserLogged() {
+        val user = auth.currentUser
+        if (user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else return
+    }
+
+    private fun toast(str: String) {
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
     }
 }
 
