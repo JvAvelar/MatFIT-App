ce86ed958115cad13fdcc80fc700c3e8d6ae3c95
João Vitor <jvitoravelar1200@gmail.com>
2025-02-27 00:28:25 -0300

    implementado o cadastro de usuarios com Firebase na CadastroActivity.kt

--- Projeto/app/src/main/java/engsoft/matfit/view/CadastroActivity.kt
+++ Projeto/app/src/main/java/engsoft/matfit/view/CadastroActivity.kt
@@ -4,10 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import engsoft.matfit.R
 import engsoft.matfit.databinding.ActivityCadastroBinding
-import engsoft.matfit.service.SharedPreferences
 
 class CadastroActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -18,36 +18,15 @@ class CadastroActivity : AppCompatActivity() {
         FirebaseAuth.getInstance()
     }
 
-    private lateinit var sharedPreferences: SharedPreferences
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        sharedPreferences = SharedPreferences(this)
         supportActionBar?.hide()
 
         binding.bntRegister.setOnClickListener {
-            saveName()
             registerUser()
-
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        verifyUserLogged()
-    }
-
-    // Verifica se o usuáro está logado após a criação e envia diretamente para a MainActivity se estiver
-    private fun verifyUserLogged(){
-       val user = auth.currentUser
-        if (user != null ){
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        } else
-            Toast.makeText(this, "Não tem usuário logado.", Toast.LENGTH_SHORT).show()
-
     }
 
     // Criação do usuário com Firebase
@@ -55,27 +34,33 @@ class CadastroActivity : AppCompatActivity() {
         val email = binding.editEmail.text.toString()
         val password = binding.editPasswd.text.toString()
 
+        if (email.isEmpty() || password.isEmpty()) {
+            toast("Email e senha obrigatórios para realizar seu cadastro.")
+            return
+        } else if (password.length < 6) {
+            toast("Sua senha deve conter pelo menos 6 caracteres.")
+            return
+        } else if (!email.contains("@")) {
+            toast("Email inválido. Verifique!")
+            return
+        }
+
         auth.createUserWithEmailAndPassword(email, password)
             .addOnSuccessListener { success ->
                 val emailSuccess = success.user?.email
-                Toast.makeText(this, "$emailSuccess foi cadastrado com sucesso!", Toast.LENGTH_LONG).show()
+                // TOAST COM LONGA DURAÇÂO
+                Toast.makeText(this, "$emailSuccess foi cadastrado com sucesso!", Toast.LENGTH_LONG)
+                    .show()
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
 
-            } .addOnFailureListener { exception ->
-                Toast.makeText(this, "Erro: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener { exception ->
+                toast("Erro: ${exception.message}")
             }
 
     }
 
-    // Verifica e salva o nome do usuário com SharedPreferences
-    private fun saveName() {
-        val name = binding.editName.text.toString()
-        if (name.isBlank()) {
-            Toast.makeText(this, getString(R.string.textFieldEmptyLogin), Toast.LENGTH_SHORT).show()
-            return
-        } else if (name.length <= 2)
-            Toast.makeText(this, getString(R.string.textNameInvalid), Toast.LENGTH_SHORT).show()
-        else {
-            sharedPreferences.saveName(name)
-        }
+    private fun toast(str: String) {
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
     }
 }
