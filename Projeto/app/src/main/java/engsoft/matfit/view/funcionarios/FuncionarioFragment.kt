4db5f1e81112b93230fb51237fc58ccd8a68bea2
João Vitor <jvitoravelar1200@gmail.com>
2025-03-01 20:02:45 -0300

    implementado a logica do progressbar no FuncionarioFragment.kt

--- Projeto/app/src/main/java/engsoft/matfit/view/funcionarios/FuncionarioFragment.kt
+++ Projeto/app/src/main/java/engsoft/matfit/view/funcionarios/FuncionarioFragment.kt
@@ -15,6 +15,7 @@ import engsoft.matfit.R
 import engsoft.matfit.databinding.FragmentFuncionarioBinding
 import engsoft.matfit.listener.OnFuncionarioListener
 import engsoft.matfit.model.Constantes
+import engsoft.matfit.util.EstadoRequisicao
 import engsoft.matfit.view.funcionarios.adapter.FuncionarioAdapter
 import engsoft.matfit.view.viewmodel.FuncionarioViewModel
 
@@ -38,33 +39,11 @@ class FuncionarioFragment : Fragment() {
 
         val listener = object : OnFuncionarioListener {
             override fun onUpdate(cpf: String) {
-                viewModel.buscarFuncionario(cpf)
-                viewModel.buscarFuncionario.observe(viewLifecycleOwner) { funcionario ->
-                    Log.i("info_onUpdateFuncionario", "Operação bem-sucedida -> $funcionario")
-                    if (funcionario != null) {
-                        val intent = Intent(context, UpdateFuncionarioActivity::class.java)
-                        intent.putExtra(Constantes.Funcionario.CPF, funcionario.cpf)
-                        intent.putExtra(Constantes.Funcionario.NOME, funcionario.nome)
-                        intent.putExtra(Constantes.Funcionario.FUNCAO, funcionario.funcao)
-                        intent.putExtra(Constantes.Funcionario.CARGA_HORARIA, funcionario.cargaHoraria)
-                        startActivity(intent)
-                    } else {
-                        Log.i("info_onUpdateFuncionario", "Erro de execução -> $funcionario")
-                        toast(getString(R.string.textFuncionarioNotFound))
-                    }
-                }
+                atualizarFuncionario(cpf)
             }
 
             override fun onDelete(cpf: String) {
-                AlertDialog.Builder(requireContext())
-                    .setTitle(getString(R.string.deleteFuncionario))
-                    .setMessage(getString(R.string.textConfirmationDelete))
-                    .setPositiveButton(getString(R.string.yes)) { dialog, which ->
-                        viewModel.deletarFuncionario(cpf)
-                    }
-                    .setNegativeButton(getString(R.string.cancel)) { dialog, which -> null }
-                    .create()
-                    .show()
+                deletarFuncionario(cpf)
             }
         }
 
@@ -97,12 +76,26 @@ class FuncionarioFragment : Fragment() {
     }
 
     private fun observadores() {
-        viewModel.listarFuncionario.observe(viewLifecycleOwner) { listFuncionario ->
-            if (listFuncionario.isNullOrEmpty())
-                mostrarMsgListaVazia()
-            else {
-                mostrarCadModel()
-                adapter.updateFuncionario(listFuncionario)
+        viewModel.estadoRequisicao.observe(viewLifecycleOwner) { estado ->
+            when (estado) {
+                is EstadoRequisicao.Carregando -> {
+                    mostrarCarregamento()
+                    Log.i("info_EstadoRequisicaoCarregando", "Carregando os dados!!")
+                }
+
+                is EstadoRequisicao.Sucesso -> {
+                    mostrarCardModel()
+                    adapter.updateFuncionario(estado.data)
+                    Log.i("info_EstadoRequisicaoSucesso", "Sucesso ao listar os funcionarios!!")
+                }
+
+                is EstadoRequisicao.Erro -> {
+                    mostrarMsgListaVazia()
+                    toast(estado.mensagem)
+                    Log.i("info_EstadoRequisicaoErro", "Erro ao listar os funcionarios!!")
+                }
+
+                else -> mostrarMsgListaVazia()
             }
         }
 
@@ -110,28 +103,73 @@ class FuncionarioFragment : Fragment() {
         viewModel.deletarFuncionario.observe(viewLifecycleOwner) { sucesso ->
             when (sucesso) {
                 false -> {
+                    Log.i("info_deletarFuncionario", "Sucesso ao deletar funcionario! -> $sucesso")
                     toast(getString(R.string.textSuccessDeleteFuncionario))
-                    viewModel.resetarDeletar()
+                    viewModel.reseteDeletar()
                 }
 
                 true -> {
+                    Log.i("info_deletarFuncionario", "Erro ao deletar funcionario! -> $sucesso")
                     toast(getString(R.string.textFailureDeleteFuncionario))
-                    viewModel.resetarDeletar()
+                    viewModel.reseteDeletar()
                 }
 
-                else -> {}
+                else -> {
+                    toast("ERRO: valor nulo!! -> $sucesso")
+                    Log.i("info_deletarFuncionario", "ERRO: valor nulo! -> $sucesso")
+                }
             }
         }
     }
 
-    private fun mostrarMsgListaVazia() {
-        binding.recyclerListFuncionario.visibility = View.GONE
-        binding.emptyView.visibility = View.VISIBLE
+    private fun atualizarFuncionario(cpf: String) {
+        viewModel.buscarFuncionario(cpf)
+        viewModel.buscarFuncionario.observe(viewLifecycleOwner) { funcionario ->
+            Log.i("info_onUpdateFuncionario", "Operação bem-sucedida -> $funcionario")
+            if (funcionario != null) {
+                val intent = Intent(context, UpdateFuncionarioActivity::class.java)
+                intent.putExtra(Constantes.Funcionario.CPF, funcionario.cpf)
+                intent.putExtra(Constantes.Funcionario.NOME, funcionario.nome)
+                intent.putExtra(Constantes.Funcionario.FUNCAO, funcionario.funcao)
+                intent.putExtra(Constantes.Funcionario.CARGA_HORARIA, funcionario.cargaHoraria)
+                startActivity(intent)
+            } else {
+                Log.i("info_onUpdateFuncionario", "Erro de execução -> $funcionario")
+                toast(getString(R.string.textFuncionarioNotFound))
+            }
+        }
     }
 
-    private fun mostrarCadModel() {
+    private fun deletarFuncionario(cpf: String) {
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.deleteFuncionario))
+            .setMessage(getString(R.string.textConfirmationDelete))
+            .setPositiveButton(getString(R.string.yes)) { dialog, which ->
+                viewModel.deletarFuncionario(cpf)
+            }
+            .setNegativeButton(getString(R.string.cancel)) { dialog, which -> null }
+            .create()
+            .show()
+    }
+
+    private fun mostrarCarregamento() {
+        binding.ProgressBar.visibility = View.VISIBLE
+        binding.emptyView.visibility = View.VISIBLE
+        binding.recyclerListFuncionario.visibility = View.GONE
+    }
+
+    private fun mostrarMsgListaVazia() {
+        binding.emptyView.visibility = View.VISIBLE
+        binding.ProgressBar.visibility = View.GONE
+        binding.recyclerListFuncionario.visibility = View.GONE
+
+    }
+
+    private fun mostrarCardModel() {
         binding.recyclerListFuncionario.visibility = View.VISIBLE
+        binding.ProgressBar.visibility = View.GONE
         binding.emptyView.visibility = View.GONE
+
     }
 
     private fun toast(msg: String) {
