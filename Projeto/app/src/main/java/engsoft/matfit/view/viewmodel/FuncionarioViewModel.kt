fdd6a8ad78b56cc07744566741c2b5dba2ba9a9c
João Vitor <jvitoravelar1200@gmail.com>
2025-03-01 20:03:27 -0300

    implementado a lógica do EstadoRequisição no FuncionarioViewModel.kt

--- Projeto/app/src/main/java/engsoft/matfit/view/viewmodel/FuncionarioViewModel.kt
+++ Projeto/app/src/main/java/engsoft/matfit/view/viewmodel/FuncionarioViewModel.kt
@@ -1,6 +1,7 @@
 package engsoft.matfit.view.viewmodel
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -8,14 +9,12 @@ import androidx.lifecycle.viewModelScope
 import engsoft.matfit.model.FuncionarioDTO
 import engsoft.matfit.model.FuncionarioUpdate
 import engsoft.matfit.service.repository.FuncionarioRepository
+import engsoft.matfit.util.EstadoRequisicao
 import kotlinx.coroutines.launch
 
 class FuncionarioViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repository = FuncionarioRepository(application.applicationContext)
-
-    private val _listarFuncionarios = MutableLiveData<List<FuncionarioDTO>>()
-    val listarFuncionario: LiveData<List<FuncionarioDTO>> = _listarFuncionarios
 
     private val _cadastro = MutableLiveData<Boolean>()
     val cadastroFuncionario: LiveData<Boolean> = _cadastro
@@ -29,49 +28,68 @@ class FuncionarioViewModel(application: Application) : AndroidViewModel(applicat
     private val _atualizar = MutableLiveData<FuncionarioDTO?>()
     val atualizarFuncionario: LiveData<FuncionarioDTO?> = _atualizar
 
+    private val _estadoRequisicao = MutableLiveData<EstadoRequisicao<List<FuncionarioDTO>>>()
+    val estadoRequisicao: LiveData<EstadoRequisicao<List<FuncionarioDTO>>> = _estadoRequisicao
 
-    fun listarFuncionarios(){
-        viewModelScope.launch {
-            _listarFuncionarios.postValue(repository.listarFuncionarios())
+
+    fun listarFuncionarios() {
+        _estadoRequisicao.postValue(EstadoRequisicao.Carregando())
+
+        try {
+            viewModelScope.launch {
+                val response = repository.listarFuncionarios()
+                if (response.isNotEmpty())
+                    _estadoRequisicao.postValue(EstadoRequisicao.Sucesso(response))
+                else
+                    _estadoRequisicao.postValue(EstadoRequisicao.Sucesso(emptyList()))
+                Log.i("info_listarFuncionarios", "Sucesso! -> $response")
+            }
+        } catch (e: Exception){
+            Log.i("info_listarFuncionarios", "Erro! -> ${e.message}")
+            _estadoRequisicao.postValue(EstadoRequisicao.Erro("Erro ao buscar funcionários!"))
         }
     }
 
-    fun cadastrarFuncionario(funcionario: FuncionarioDTO){
+    fun cadastrarFuncionario(funcionario: FuncionarioDTO) {
         viewModelScope.launch {
             _cadastro.postValue(repository.cadastrarFuncionario(funcionario))
         }
     }
 
-    fun buscarFuncionario(cpf: String){
+    fun buscarFuncionario(cpf: String) {
         viewModelScope.launch {
             try {
+                Log.i("info_buscarFuncionarios", "Sucesso ao buscar funcionario! ")
                 _buscar.postValue(repository.buscarFuncionario(cpf))
-            } catch (e: Exception){
+            } catch (e: Exception) {
+                Log.i("info_buscarFuncionarios", "Erro na busca de funcionario-> ${e.message}")
                 _buscar.postValue(null)
                 e.printStackTrace()
             }
         }
     }
 
-    fun atualizarFuncionario(cpf: String, funcionario: FuncionarioUpdate){
+    fun atualizarFuncionario(cpf: String, funcionario: FuncionarioUpdate) {
         viewModelScope.launch {
-            try{
+            try {
+                Log.i("info_atualizarFuncionarios", "Sucesso ao atualizar funcionario! ")
                 _atualizar.postValue(repository.atualizarFuncionario(cpf, funcionario))
-            } catch (e: Exception){
+            } catch (e: Exception) {
+                Log.i("info_atualizarFuncionarios", "Erro ao atualizar funcionario! -> ${e.message}")
                 _atualizar.postValue(null)
                 e.printStackTrace()
             }
         }
     }
 
-    fun deletarFuncionario(cpf:String){
+    fun deletarFuncionario(cpf: String) {
         viewModelScope.launch {
             _deletar.postValue(repository.deletarFuncionario(cpf))
             listarFuncionarios()
         }
     }
 
-    fun resetarDeletar(){
+    fun reseteDeletar() {
         _deletar.postValue(null)
     }
 }
