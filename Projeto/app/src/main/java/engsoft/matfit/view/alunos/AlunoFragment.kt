f27330cf9fe52c306eb52303208422c5effa8f18
João Vitor <jvitoravelar1200@gmail.com>
2025-03-01 16:12:13 -0300

    implementado a logica para fazer download do arquivo excel e a logica da progress bar quando for necessário esperar algum tempo na AlunoFragment.kt

--- Projeto/app/src/main/java/engsoft/matfit/view/alunos/AlunoFragment.kt
+++ Projeto/app/src/main/java/engsoft/matfit/view/alunos/AlunoFragment.kt
@@ -8,14 +8,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.TooltipCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import engsoft.matfit.R
 import engsoft.matfit.databinding.FragmentAlunoBinding
 import engsoft.matfit.listener.OnAlunoListener
+import engsoft.matfit.model.Aluno
 import engsoft.matfit.model.Constantes
+import engsoft.matfit.util.EstadoRequisicao
 import engsoft.matfit.view.alunos.adapter.AlunoAdapter
 import engsoft.matfit.view.viewmodel.AlunoViewModel
 
@@ -26,6 +30,23 @@ class AlunoFragment : Fragment() {
     private lateinit var viewModel: AlunoViewModel
     private val adapter = AlunoAdapter()
 
+    private var listaAlunosExcel = emptyList<Aluno>()
+
+    private val salvarArquivoLauncher = registerForActivityResult(
+        ActivityResultContracts.CreateDocument("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    ) { uri ->
+        uri?.let {
+            viewModel.escreverExcel(it, requireContext(), listaAlunosExcel)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnDownload.setOnClickListener {
+            fazerDownloadArquivo()
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,42 +55,23 @@ class AlunoFragment : Fragment() {
         _binding = FragmentAlunoBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this)[AlunoViewModel::class.java]
 
-        // Layout
+        // Layout e Adapter
         binding.recyclerListAlunos.layoutManager = LinearLayoutManager(context)
-        // Adapter
         binding.recyclerListAlunos.adapter = adapter
+        // Mostra o texto ao passar o dedo no botão de download
+        TooltipCompat.setTooltipText(binding.btnDownload, "Exportar dados")
 
         val listener = object : OnAlunoListener {
             override fun onUpdate(cpf: String) {
-                clickAtualizar(cpf)
+                atualizarAluno(cpf)
             }
 
             override fun onDelete(cpf: String) {
-                AlertDialog.Builder(requireContext()).setTitle(getString(R.string.deleteAluno))
-                    .setMessage(getString(R.string.textConfirmationDelete))
-                    .setPositiveButton(getString(R.string.yes)) { dialog, which ->
-                        viewModel.deletarAluno(cpf)
-                    }.setNegativeButton(getString(R.string.cancel)) { dialog, which -> null }
-                    .create().show()
+                deletarAluno(cpf)
             }
 
             override fun OnPayment(cpf: String) {
-                viewModel.verificarPagamento(cpf)
-                viewModel.verificarPagamento.observe(viewLifecycleOwner) { aluno ->
-                    if(aluno != null) {
-                        Log.i("info_OnPaymentAluno", "Operação bem-sucedida -> $aluno")
-                        val intent = Intent(context, PagamentoAlunoActivity::class.java)
-                        intent.putExtra(Constantes.Aluno.CPF, aluno.cpf)
-                        intent.putExtra(Constantes.Aluno.NOME, aluno.nome)
-                        intent.putExtra(Constantes.Aluno.ESPORTE, aluno.esporte)
-                        intent.putExtra(Constantes.Aluno.DATA_PAGAMENTO, aluno.dataPagamento)
-                        intent.putExtra(Constantes.Aluno.PAGAMENTO_ATRASADO, aluno.pagamentoAtrasado)
-                        startActivity(intent)
-                    } else {
-                        Log.i("info_OnPaymentAluno", "Erro de execução -> $aluno")
-                        toast(getString(R.string.textAlunoNotFound))
-                    }
-                }
+                verificarPagamento(cpf)
             }
         }
 
@@ -78,18 +80,10 @@ class AlunoFragment : Fragment() {
         }
 
         viewModel.listarAlunos()
-
         adapter.attachListener(listener)
-
         observadores()
 
         return binding.root
-    }
-
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 
     override fun onResume() {
@@ -97,63 +91,146 @@ class AlunoFragment : Fragment() {
         viewModel.listarAlunos()
     }
 
-    private fun clickAtualizar(cpf: String) {
-        viewModel.buscarAluno(cpf)
-        viewModel.buscarAluno.observe(viewLifecycleOwner) { aluno ->
-            Log.i("info_onUpdate", "Bem sucedido -> $aluno")
-            if (aluno != null) {
-                val intent = Intent(context, UpdateAlunoActivity::class.java)
-                intent.putExtra(Constantes.Aluno.CPF, aluno.cpf)
-                intent.putExtra(Constantes.Aluno.NOME, aluno.nome)
-                intent.putExtra(Constantes.Aluno.ESPORTE, aluno.esporte)
-                startActivity(intent)
-            } else {
-                Log.i("info_onUpdate", "Erro de execução -> $aluno")
-                toast(getString(R.string.textAlunoNotFound))
-            }
-        }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     @SuppressLint("SetTextI18n")
     private fun observadores() {
-        viewModel.listarAlunos.observe(viewLifecycleOwner) { listaAlunos ->
-            if (listaAlunos.isNullOrEmpty())
-                mostrarMsgListaVazia()
-            else{
-                mostrarCadModel()
-                adapter.updateAlunos(listaAlunos)
-            }
-        }
-
-        // valores invertidos para sucesso
         viewModel.deletar.observe(viewLifecycleOwner) { sucesso ->
             when (sucesso) {
                 false -> {
+                    Log.i("info_deletar", "sucesso ao deletar aluno! -> $sucesso")
                     toast(getString(R.string.textSucessDeletedAluno))
-                    viewModel.resetarDeletar()
+                    viewModel.reseteDeletar()
                 }
 
                 true -> {
+                    Log.i("info_deletar", "falha ao deletar aluno! -> $sucesso")
                     toast(getString(R.string.textFailureDeletedAluno))
-                    viewModel.resetarDeletar()
+                    viewModel.reseteDeletar()
                 }
 
-                else -> {}
+                else -> {
+                    Log.i("info_deletar", "ERRO: valor nulo! -> $sucesso")
+                }
+            }
+        }
+
+        // Listagem de alunos com o estado de requisição para gerenciar o ProgressBar
+        viewModel.estadoRequisicao.observe(viewLifecycleOwner) { estado ->
+            when (estado) {
+                is EstadoRequisicao.Carregando -> {
+                    mostrarCarregamento()
+                    Log.i("info_EstadoRequisicaoCarregando", "Carregando os dados!!")
+                }
+
+                is EstadoRequisicao.Sucesso -> {
+                    val alunos = estado.data
+                    adapter.updateAlunos(alunos)
+                    listaAlunosExcel = alunos
+                    mostrarCardModel()
+                    Log.i("info_EstadoRequisicaoSucesso", "Sucesso no listar Alunos! -> $alunos ")
+                }
+
+                is EstadoRequisicao.Erro -> {
+                    mostrarMsgListaVazia()
+                    toast(estado.mensagem)
+                    Log.i(
+                        "info_EstadoRequisicaoErro",
+                        "Erro ao listar alunos! -> ${estado.mensagem} "
+                    )
+                }
+
+                else -> mostrarMsgListaVazia()
             }
         }
     }
 
-    private fun mostrarMsgListaVazia() {
-        binding.recyclerListAlunos.visibility = View.GONE
-        binding.emptyView.visibility = View.VISIBLE
+    private fun fazerDownloadArquivo() {
+        viewModel.estadoRequisicao.value?.let { estado ->
+            when (estado) {
+                is EstadoRequisicao.Carregando -> {
+                    binding.ProgressBar.visibility = View.VISIBLE
+                }
+
+                is EstadoRequisicao.Sucesso -> {
+                    Log.i("info_btnDownload", "fazer Download do arquivo")
+                    listaAlunosExcel = estado.data
+                    viewModel.exportarAlunosParaExcel(listaAlunosExcel, salvarArquivoLauncher)
+                }
+
+                is EstadoRequisicao.Erro -> {
+                    toast(estado.mensagem)
+                }
+            }
+        }
     }
 
-    private fun mostrarCadModel(){
+    private fun deletarAluno(cpf: String) {
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.deleteAluno))
+            .setMessage(getString(R.string.textConfirmationDelete))
+            .setPositiveButton(getString(R.string.yes)) { _, _ ->
+                viewModel.deletarAluno(cpf)
+            }
+            .setNegativeButton(getString(R.string.cancel), null)
+            .create().show()
+    }
+
+    private fun verificarPagamento(cpf: String) {
+        viewModel.verificarPagamento(cpf)
+        viewModel.verificarPagamento.observe(viewLifecycleOwner) { aluno ->
+            aluno?.let {
+                Log.i("info_OnPaymentAluno", "Operação bem-sucedida -> $it")
+                val intent = Intent(context, PagamentoAlunoActivity::class.java).apply {
+                    putExtra(Constantes.Aluno.CPF, it.cpf)
+                    putExtra(Constantes.Aluno.NOME, it.nome)
+                    putExtra(Constantes.Aluno.ESPORTE, it.esporte)
+                    putExtra(Constantes.Aluno.DATA_PAGAMENTO, it.dataPagamento)
+                    putExtra(Constantes.Aluno.PAGAMENTO_ATRASADO, it.pagamentoAtrasado)
+                }
+                startActivity(intent)
+            } ?: toast(getString(R.string.textAlunoNotFound))
+        }
+    }
+
+    private fun atualizarAluno(cpf: String) {
+        viewModel.buscarAluno(cpf)
+        viewModel.buscarAluno.observe(viewLifecycleOwner) { aluno ->
+            Log.i("info_clickAtualizar", "observeBuscarAluno -> $aluno")
+            aluno?.let {
+                val intent = Intent(context, UpdateAlunoActivity::class.java).apply {
+                    putExtra(Constantes.Aluno.CPF, it.cpf)
+                    putExtra(Constantes.Aluno.NOME, it.nome)
+                    putExtra(Constantes.Aluno.ESPORTE, it.esporte)
+                }
+                startActivity(intent)
+            } ?: toast(getString(R.string.textAlunoNotFound))
+        }
+    }
+
+    private fun mostrarCarregamento() {
+        binding.ProgressBar.visibility = View.VISIBLE
+        binding.emptyView.visibility = View.VISIBLE
+        binding.recyclerListAlunos.visibility = View.GONE
+    }
+
+    private fun mostrarCardModel() {
         binding.recyclerListAlunos.visibility = View.VISIBLE
         binding.emptyView.visibility = View.GONE
+        binding.ProgressBar.visibility = View.GONE
+    }
+
+    private fun mostrarMsgListaVazia() {
+        binding.emptyView.visibility = View.VISIBLE
+        binding.recyclerListAlunos.visibility = View.GONE
+        binding.ProgressBar.visibility = View.GONE
     }
 
     private fun toast(msg: String) {
         Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
     }
+
 }
