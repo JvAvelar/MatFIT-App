2ed51009bd48ff347d7f2144e9b557b20dcb15c5
João Vitor <jvitoravelar1200@gmail.com>
2025-03-04 18:34:49 -0300

    adicionado validação no método buscarEquipamento na EquipamentoRepository.kt

--- Projeto/app/src/main/java/engsoft/matfit/service/repository/EquipamentoRepository.kt
+++ Projeto/app/src/main/java/engsoft/matfit/service/repository/EquipamentoRepository.kt
@@ -33,14 +33,14 @@ class EquipamentoRepository {
     }
 
     suspend fun buscarEquipamento(id: Int): EquipamentoDTO? {
-        var equipamento: EquipamentoDTO? = null
-        try {
+       return  try {
             val retorno = remote.buscarEquipamento(id)
-            if(retorno.isSuccessful){
+            if (retorno.isSuccessful) {
                 retorno.body()?.let {
-                    Log.i("info_buscarEquipamento", "Operação bem-sucedida: $it")
-                    equipamento = it
-                    return equipamento
+                    if (it.id == id) {
+                        Log.i("info_buscarEquipamento", "Operação bem-sucedida: $it")
+                        it
+                    } else Log.i("info_buscarEquipamento", "ID retornado não corresponde ao solicitado")
                 }
             } else {
                 Log.i(
@@ -48,71 +48,75 @@ class EquipamentoRepository {
                     "Erro na operação: = ${retorno.code()} - ${retorno.message()}"
                 )
             }
+            null
         } catch (e: Exception) {
             Log.i("info_buscarEquipamento", "Falhou -> ${e.message}")
             e.printStackTrace()
-        }
-        return equipamento
+           null
+       }
     }
 
-    suspend fun atualizarEquipamento(id: Int, equipamento: EquipamentoDTO): EquipamentoDTO?{
+    suspend fun atualizarEquipamento(id: Int, equipamento: EquipamentoDTO): EquipamentoDTO? {
         var equipamentoRetornado: EquipamentoDTO? = null
         try {
             val retorno = remote.atualizarEquipamento(id, equipamento)
-            if (retorno.isSuccessful){
+            if (retorno.isSuccessful) {
                 retorno.body()?.let {
                     Log.i("info_atualizarEquipamento", "Operação bem-sucedida: $it")
                     equipamentoRetornado = it
                     return equipamentoRetornado
                 }
-            } else{
+            } else {
                 Log.i(
                     "info_atualizarEquipamento",
                     "Erro na operação: = ${retorno.code()} - ${retorno.message()}"
                 )
             }
-        }catch (e: Exception){
+        } catch (e: Exception) {
             Log.i("info_atualizarEquipamento", "Falhou -> ${e.message}")
             e.printStackTrace()
         }
         return equipamentoRetornado
     }
 
-    suspend fun deletarEquipamento(id: Int): Boolean{
+    suspend fun deletarEquipamento(id: Int): Boolean {
         var sucesso = false
         try {
             val retorno = remote.deletarEquipamento(id)
-            if (retorno.isSuccessful){
+            if (retorno.isSuccessful) {
                 retorno.body()?.let {
                     Log.i("info_deletarEquipamento", "Operação bem-sucedida: $it")
                     sucesso = it
                     return sucesso
                 }
-            } else{
+            } else {
                 Log.i(
                     "info_deletarEquipamento",
                     "Erro na operação: = ${retorno.code()} - ${retorno.message()}"
                 )
             }
-        } catch (e: Exception){
+        } catch (e: Exception) {
             Log.i("info_deletarEquipamento", "Falhou -> ${e.message}")
             e.printStackTrace()
         }
         return sucesso
     }
 
-    suspend fun listarEquipamentos(): List<EquipamentoDTO>{
+    suspend fun listarEquipamentos(): List<EquipamentoDTO> {
         try {
             val retorno = remote.listarEquipamentos()
-            if(retorno.isSuccessful){
+            if (retorno.isSuccessful) {
                 retorno.body()?.let { list ->
                     Log.i("info_listarEquipamentos", "Operação bem-sucedida: $list")
                     return list
                 }
-            } else{
-                Log.i("info_listarEquipamentos", "Erro na operação -> ${retorno.code()} - ${retorno.message()}")
+            } else {
+                Log.i(
+                    "info_listarEquipamentos",
+                    "Erro na operação -> ${retorno.code()} - ${retorno.message()}"
+                )
             }
-        } catch (e: Exception){
+        } catch (e: Exception) {
             Log.i("info_listarEquipamentos", "Falhou -> ${e.message}")
             e.printStackTrace()
         }
