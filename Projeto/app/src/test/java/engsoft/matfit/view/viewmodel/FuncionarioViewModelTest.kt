0c097c23e452ede31d941304026ca0b88f35c3fd
João Vitor <jvitoravelar1200@gmail.com>
2025-03-24 09:47:22 -0300

    implementado os métodos de testes na classe FuncionarioViewModelTest.kt

--- Projeto/app/src/test/java/engsoft/matfit/view/viewmodel/FuncionarioViewModelTest.kt
+++ Projeto/app/src/test/java/engsoft/matfit/view/viewmodel/FuncionarioViewModelTest.kt
@@ -1,15 +1,24 @@
 package engsoft.matfit.view.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import engsoft.matfit.model.FuncionarioDTO
+import engsoft.matfit.model.FuncionarioUpdate
 import engsoft.matfit.service.repository.FuncionarioRepository
+import engsoft.matfit.util.EstadoRequisicao
 import engsoft.matfit.util.RegraTestCustomizada
+import engsoft.matfit.util.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.whenever
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class FuncionarioViewModelTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
@@ -34,22 +43,186 @@ class FuncionarioViewModelTest {
 
 
     @Test
-    fun listarFuncionarios() = runTest {
+    fun listarFuncionarios_sucesso_retornaListaFuncionario() = runTest {
+        // DADO -> sucesso
+        val listaEsperada = listOf(
+            FuncionarioDTO("592.829.630-47", "Zyon Avelar", "Eletricista", 20),
+            FuncionarioDTO("120.454.000-42", "Luana Duarte", "Personal", 20),
+            FuncionarioDTO("837.709.180-10", "Luiz alves", "Segurança", 20),
+        )
+        whenever(mockRepository.listarFuncionarios())
+            .thenReturn(listaEsperada)
+
+        // QUANDO
+        viewModel.listarFuncionarios()
+        advanceUntilIdle()
+
+        // ENTÃO
+        val estado = viewModel.estadoRequisicao.getOrAwaitValue()
+        assertThat(estado).isNotNull()
+        assertThat(estado).isInstanceOf(EstadoRequisicao.Sucesso::class.java)
+        assertThat((estado as EstadoRequisicao.Sucesso).data).isEqualTo(listaEsperada)
     }
 
     @Test
-    fun cadastrarFuncionario() = runTest{
+    fun listarFuncionario_falha_retornaListaVazia() = runTest {
+        // DADO -> falha
+        whenever(mockRepository.listarFuncionarios())
+            .thenThrow(RuntimeException("Erro! Lista Vazia!!"))
+
+        // QUANDO
+        viewModel.listarFuncionarios()
+        advanceUntilIdle()
+
+        // ENTÃO
+        val estado = viewModel.estadoRequisicao.getOrAwaitValue()
+        assertThat(estado).isNotNull()
+        assertThat(estado).isInstanceOf(EstadoRequisicao.Erro::class.java)
+        assertThat((estado as EstadoRequisicao.Erro).mensagem).isEqualTo("Lista vazia!")
     }
 
     @Test
-    fun buscarFuncionario() = runTest {
+    fun cadastrarFuncionario_sucesso_retornaTrue() = runTest {
+        // DADO -> sucesso
+        val cpf = "592.829.630-47"
+        val funcionario = FuncionarioDTO(cpf, "Joao Vitor", "Personal", 20)
+        whenever(mockRepository.cadastrarFuncionario(funcionario))
+            .thenReturn(true)
+
+        // QUANDO
+        viewModel.cadastrarFuncionario(funcionario)
+        advanceUntilIdle()
+
+        // ENTÃO
+        val resultado = viewModel.cadastroFuncionario.getOrAwaitValue()
+        assertThat(resultado).isNotNull()
+        assertThat(resultado).isTrue()
     }
 
     @Test
-    fun atualizarFuncionario() = runTest {
+    fun cadastrarFuncionario_falha_retornaFalse() = runTest {
+        // DADO -> falha
+        val cpf = "123.456.789-10"
+        val funcionario = FuncionarioDTO(cpf, "Joao Vitor", "Personal", 20)
+        whenever(mockRepository.cadastrarFuncionario(funcionario))
+            .thenThrow(RuntimeException("CPF inválido!"))
+
+        // QUANDO
+        viewModel.cadastrarFuncionario(funcionario)
+        advanceUntilIdle()
+
+        // ENTÃO
+        val resultado = viewModel.cadastroFuncionario.getOrAwaitValue()
+        assertThat(resultado).isNotNull()
+        assertThat(resultado).isFalse()
     }
 
     @Test
-    fun deletarFuncionario() = runTest {
+    fun buscarFuncionario_sucesso_retornaFuncionario() = runTest {
+        // DADO -> sucesso
+        val cpf = "592.829.630-47"
+        val funcionarioEsperado = FuncionarioDTO(
+            "592.829.630-47", "Zyon Avelar",
+            "Eletricista", 20
+        )
+        whenever(mockRepository.buscarFuncionario(cpf))
+            .thenReturn(funcionarioEsperado)
+
+        // QUANDO
+        viewModel.buscarFuncionario(cpf)
+        advanceUntilIdle()
+
+        // ENTÃO
+        val resultado = viewModel.buscarFuncionario.getOrAwaitValue()
+        assertThat(resultado).isNotNull()
+        assertThat(resultado).isEqualTo(funcionarioEsperado)
+    }
+
+    @Test
+    fun buscarFuncionario_falaha_retornaNull() = runTest {
+        // DADO -> falha
+        val cpf = "123.456.789-10"
+        whenever(mockRepository.buscarFuncionario(cpf))
+            .thenThrow(RuntimeException("CPF inválido!"))
+
+        // QUANDO
+        viewModel.buscarFuncionario(cpf)
+        advanceUntilIdle()
+
+        // ENTÃO
+        val resultado = viewModel.buscarFuncionario.getOrAwaitValue()
+        assertThat(resultado).isNull()
+    }
+
+    @Test
+    fun atualizarFuncionario_sucesso_retornaFuncionario() = runTest {
+        // DADO -> sucesso
+        val cpf = "592.829.630-47"
+        val funcionarioEsperado =  FuncionarioDTO(
+            "592.829.630-47", "Zyon Avelar",
+            "Segurança", 20
+        )
+        val dadosAtualizados = FuncionarioUpdate("", "Segurança", 20)
+        whenever(mockRepository.atualizarFuncionario(cpf, dadosAtualizados))
+            .thenReturn(funcionarioEsperado)
+
+        // QUANDO
+        viewModel.atualizarFuncionario(cpf, dadosAtualizados)
+        advanceUntilIdle()
+
+        // ENTÃO
+        val resultado = viewModel.atualizarFuncionario.getOrAwaitValue()
+        assertThat(resultado).isNotNull()
+        assertThat(resultado).isEqualTo(funcionarioEsperado)
+    }
+
+    @Test
+    fun atualizarFuncionario_falha_retornaNull() = runTest {
+        // DADO -> falha
+        val cpf = "592.829.630-47"
+        val dadosAtualizados = FuncionarioUpdate("", "Segurança", 0)
+        whenever(mockRepository.atualizarFuncionario(cpf, dadosAtualizados))
+            .thenThrow(RuntimeException("Carga horária invalida!"))
+
+        // QUANDO
+        viewModel.atualizarFuncionario(cpf, dadosAtualizados)
+        advanceUntilIdle()
+
+        // ENTÃO
+        val resultado = viewModel.atualizarFuncionario.getOrAwaitValue()
+        assertThat(resultado).isNull()
+    }
+
+    @Test
+    fun deletarFuncionario_sucesso_retornaTrue() = runTest {
+        // DADO -> sucesso
+        val cpf = "592.829.630-47"
+        whenever(mockRepository.deletarFuncionario(cpf))
+            .thenReturn(true)
+
+        // QUANDO
+        viewModel.deletarFuncionario(cpf)
+        advanceUntilIdle()
+
+        // ENTÃO
+        val resultado = viewModel.deletarFuncionario.getOrAwaitValue()
+        assertThat(resultado).isNotNull()
+        assertThat(resultado).isTrue()
+    }
+
+    @Test
+    fun deletarFuncionario_falha_retornaNull() = runTest {
+        // DADO -> falha
+        val cpf = "123.456.789-10"
+        whenever(mockRepository.deletarFuncionario(cpf))
+            .thenThrow(RuntimeException("CPF inválido!!"))
+
+        // QUANDO
+        viewModel.deletarFuncionario(cpf)
+        advanceUntilIdle()
+
+        // ENTÃO
+        val resultado = viewModel.deletarFuncionario.getOrAwaitValue()
+        assertThat(resultado).isNull()
     }
 }
